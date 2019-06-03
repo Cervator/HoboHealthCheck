790927179a80c125509f42a3eb0225bfb73ccdbc
Cervator <cervator@gmail.com>
2019-06-02 23:38:30 -0400

    Add some logic

--- vars/hoboWebCheck.groovy
+++ vars/hoboWebCheck.groovy
@@ -3,5 +3,16 @@
 def call(String url, String phrase) {
   echo "Going to check URL $url and look for the phrase $phrase"
 
-  return false
+  def data
+  try {
+    data = new URL(url).getText()
+  } catch (Exception e) {}
+  println "We got data: $data"
+  if (data == null || !data.contains("Cool Jenkins X demo")) {
+    println "Hoboville calls for aid! Something is awry!"
+    return false
+  } else {
+    println "All is well in Hoboville!"
+    return true
+  }
 }
